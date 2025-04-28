5d9ee4b86719b6cd27ac5897b92074bfb02b88f1
IsaacWilliams101 <isaacwilliams101@outlook.com>
2025-04-28 09:29:15 +0100

    Add files via upload

---
+++ GPS.py
@@ -0,0 +1,21 @@
+import gps
+import time
+try:
+    session = gps.gps(mode=gps.WATCH_ENABLE)
+    while 0 == session.read():
+        if not (gps.MODE_SET & session.valid):
+            continue
+        print('Mode: %s(%d) Time: ' %
+              (("Invalid", "NO_FIX", "2D", "3D")[session.fix.mode],
+               session.fix.mode), end="")
+        print(session.fix.time, end="")
+        if ((gps.isfinite(session.fix.latitude)) and
+             gps.isfinite(session.fix.longitude)):
+            print(" Lat %.6f Lon %.6f" %
+                  (session.fix.latitude, session.fix.longitude))
+        else:
+            print(" Lat n/a Lon n/a")    
+except KeyboardInterrupt:
+    print('')
+session.close()
+exit(0)
